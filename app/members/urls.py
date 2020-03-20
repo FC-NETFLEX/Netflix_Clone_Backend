c086140ebba6b4aa9a88a0029ef9acfcd7100544
parkhongbeen <pack122@naver.com>
2020-03-20 19:54:58 +0900

    feat: url수정

--- app/members/urls.py
+++ app/members/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from members.apis import GetUserInfoAPIView, AuthTokenAPIView, LogoutAPIView, CreateUserAPIView
 
 urlpatterns = [
-    path('get-user/', GetUserInfoAPIView.as_view()),  # 유저의 정보 확인
-    path('auth-token/', LogoutAPIView.as_view()),  # 유저 로그인
-    path('create-user/', CreateUserAPIView.as_view()),  # 회원가입
-    path('logout-user/', LogoutAPIView.as_view()),  # 유저 로그아웃
+    path('', CreateUserAPIView.as_view()),  # 회원가입
+    path('login/', AuthTokenAPIView.as_view()),  # 유저 로그인
+    path('logout/', LogoutAPIView.as_view()),  # 유저 로그아웃
 ]
