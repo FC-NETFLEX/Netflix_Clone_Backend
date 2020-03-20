137845422af1b4328b9c20c6bf6592fa560ae93d
hbyyy <qjaduddl94@gmail.com>
2020-03-20 20:27:56 +0900

    feat: members 관리 api 추가 1. login, logout, create 추가

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from members.apis import GetUserInfoAPIView, AuthTokenAPIView, LogoutAPIView, CreateUserAPIView
+from members.apis import LogoutAPIView, CreateUserAPIView, UserLoginAPIView
 
 urlpatterns = [
     path('', CreateUserAPIView.as_view()),  # 회원가입
-    path('login/', AuthTokenAPIView.as_view()),  # 유저 로그인
+    path('login/', UserLoginAPIView.as_view()),  # 유저 로그인
     path('logout/', LogoutAPIView.as_view()),  # 유저 로그아웃
 ]
