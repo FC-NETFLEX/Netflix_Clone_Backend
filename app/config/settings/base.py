d445d307d36fe590acab322f7262c2020a221809
hbyyy <qjaduddl94@gmail.com>
2020-03-22 22:57:59 +0900

    feat: RDS 환경에서 테스트

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -93,12 +93,7 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = SECRETS['DATABASES']
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
