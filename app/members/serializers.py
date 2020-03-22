5fed393e4ce2fc156c0764cac2a6d05c0f41abe0
hbyyy <qjaduddl94@gmail.com>
2020-03-22 20:44:38 +0900

    feat: members api generic view 이용해 구현

--- app/members/serializers.py
+++ app/members/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 from members.models import User, Profile
 
@@ -7,14 +8,24 @@ class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
 
+    def to_representation(self, instance):
+        token = Token.objects.get(user=instance)
+        return {
+            "id": instance.id,
+            "email": instance.email,
+            "token": token.key
+        }
+
 
 class UserDetailSerializer(serializers.ModelSerializer):
     profile = serializers.PrimaryKeyRelatedField(many=True, queryset=Profile.objects.all())
+    token = serializers.PrimaryKeyRelatedField(source='auth_token', queryset=Token.objects.all())
 
     class Meta:
         model = User
-        fields = ['id', 'email', 'profile']
+        fields = ['id', 'email', 'token', 'profile']
