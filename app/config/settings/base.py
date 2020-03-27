ebcb0d1afad0e305e447bc9d56325a382b67c139
hbyyy <qjaduddl94@gmail.com>
2020-03-27 15:35:19 +0900

    feat: drf-yasg 적용'

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'storages',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- app/config/urls.py
+++ app/config/urls.py
@@ -17,6 +17,22 @@ import os
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Netflex API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns_api = [
     path('members/', include('members.urls')),
@@ -25,6 +41,9 @@ urlpatterns_api = [
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(urlpatterns_api)),
+    path('swagger<str:format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.develop':
