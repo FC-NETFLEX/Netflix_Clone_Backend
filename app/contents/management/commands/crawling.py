866738f3b28c4f0db4a75f9ab47f2d5c12ee283e
parkhongbeen <pack122@naver.com>
2020-03-30 17:11:20 +0900

    feat: crawling초기데이터 생성 커맨드 구현

--- app/contents/management/commands/crawling.py
+++ app/contents/management/commands/crawling.py
@@ -1,14 +1,45 @@
 from django.core.management.base import BaseCommand
-
 from contents.management.commands.crawler import get_url, get_item
+import mimetypes
+import requests
+import magic
+from django.core.files.uploadedfile import SimpleUploadedFile
+from contents.models import Contents, Category, Actor, Director
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        pass
 
     def handle(self, *args, **options):
-        for movie_url in get_url():
-            get_item(movie_url)
+        url_list = get_url()
+
+        for url in url_list:
+            item = get_item(url)
+            contents, is_create = Contents.objects.get_or_create(contents_title=item['title'])
+
+            if is_create:
+                response = requests.get(item['image_url'])
+                binary_data = response.content
+                mime_type = magic.from_buffer(binary_data, mime=True)
+                ext = mimetypes.guess_extension(mime_type)
+                file = SimpleUploadedFile(f'{item["title"]}{ext}', binary_data)
+
+                contents.contents_summary = item['summary']
+                contents.contents_title_english = item['title_english']
+                contents.contents_image = file
+                contents.contents_rating = item['rating']
+                contents.contents_length = item['length']
+                contents.contents_pub_year = item['pub_year']
+                contents.save()
+
+                for category in item['genre']:
+                    c1, _ = Category.objects.get_or_create(category_name=category)
+
+                for actor in item['actor']:
+                    a1, _ = Actor.objects.get_or_create(actor_name=actor)
+                    contents.actors.add(a1)
+
+                for director in item['director']:
+                    d1, _ = Director.objects.get_or_create(director_name=director)
+                    contents.directors.add(d1)
 
         return self.stdout.write('크롤링 완료')
