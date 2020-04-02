4c1209e3286339d18b02d07cfcffa3b9c75d4d86
hbyyy <qjaduddl94@gmail.com>
2020-04-02 14:35:17 +0900

    feat: home api 수정 중 : url로 주는 프로필이나 컨텐츠가 없을 때 오류 처리

--- app/contents/apis.py
+++ app/contents/apis.py
@@ -1,7 +1,8 @@
 import random
 
 from django.db.models import Max
-from rest_framework import status, generics
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -45,8 +46,8 @@ class ContentsRetrieveListView(APIView):
 
 class ContentsLikeAPIView(APIView):
     def get(self, request, profile_pk, contents_pk):
-        profile = Profile.objects.get(pk=profile_pk)
-        contents = Contents.objects.get(pk=contents_pk)
+        profile = get_object_or_404(Profile, pk=profile_pk)
+        contents = get_object_or_404(Contents, pk=contents_pk)
 
         if profile in contents.like_profiles.all():
             contents.like_profiles.remove(profile)
@@ -58,8 +59,8 @@ class ContentsLikeAPIView(APIView):
 
 class ContentsSelectAPIView(APIView):
     def get(self, request, profile_pk, contents_pk):
-        profile = Profile.objects.get(pk=profile_pk)
-        contents = Contents.objects.get(pk=contents_pk)
+        profile = get_object_or_404(Profile, pk=profile_pk)
+        contents = get_object_or_404(Contents, pk=contents_pk)
 
         if profile in contents.select_profiles.all():
             contents.select_profiles.remove(profile)
@@ -69,40 +70,16 @@ class ContentsSelectAPIView(APIView):
         return Response(status=status.HTTP_200_OK)
 
 
-# class ContentsListView(generics.ListAPIView):
-#     serializer_class = ContentsSerializer
-#     queryset = Contents.objects.all()
-#
-#     def list(self, request, *args, **kwargs):
-#         all_contents = self.get_queryset()
-#         recommand_contents = all_contents.filter(contents_pub_year='2020')[:10]
-#         top_contents = get_top_contents()
-#         ad_contents = get_ad_contents()
-#         preview_contents = Contents.objects.all()[:10]
-#
-#         serializer_all = self.get_serializer(all_contents, many=True)
-#         serializer_recommand = self.get_serializer(recommand_contents, many=True)
-#         serializer_preview = self.get_serializer(preview_contents, many=True)
-#         serializer_top = ContentsDetailSerializer(top_contents)
-#         serializer_ad = ContentsDetailSerializer(ad_contents)
-#
-#         data = {
-#             "top_contents": serializer_top.data,
-#             "ad_contents": serializer_ad.data,
-#             "recommand_contents": serializer_recommand.data,
-#             "preview_contents": serializer_preview.data,
-#             "all_contents": serializer_all.data
-#         }
-#         return Response(data)
 class ContentsListView(APIView):
 
     def get(self, request, profile_pk):
+        profile = get_object_or_404(Profile, pk=profile_pk)
         all_contents = Contents.objects.all()
         recommand_contents = all_contents.filter(contents_pub_year='2020')[:10]
         top_contents = get_top_contents()
         ad_contents = get_ad_contents()
         preview_contents = Contents.objects.all()[:10]
-        watching_video = Watching.objects.filter(profile__id=profile_pk)
+        watching_video = Watching.objects.filter(profile=profile)
 
         serializer_all = ContentsSerializer(all_contents, many=True)
         serializer_recommand = ContentsSerializer(recommand_contents, many=True)
@@ -120,4 +97,3 @@ class ContentsListView(APIView):
             "watcing_video": serializer_watching_video.data
         }
         return Response(data)
-
