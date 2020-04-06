8d377e89c748a5c21ebd4dd365f1f4c60c128486
BeomYeongHeo <37058561+hbyyy@users.noreply.github.com>
2020-04-06 22:20:29 +0900

    Fix/crawling (#68)
    * feat: get_page_url() 구현
    
    * feat: get_url 함수 변경 (page_url 을 인수로 받도록 변경)
    
    * feat: get_item에서 title 이 없거나, 관람 등급이 기준에 맞지 않는 컨텐츠ㅌ는 크롤링하지 않도록 수정
    
    * feat: 데이터 크롤링 수정
    1. 크롤링해오는 데이터 양 늘림 (500개)
    2. 네이버 영화의 pagenation 된 페이지들을 크롤링해 올 수 있도록 수정

--- app/contents/management/commands/crawler.py
+++ app/contents/management/commands/crawler.py
@@ -1,63 +1,73 @@
 from urllib import parse
-from bs4 import BeautifulSoup
-import requests
-import magic
 
-def get_url():
-    url = r'https://movie.naver.com/movie/sdb/rank/rmovie.nhn'
-    req = requests.get(url)
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_page_url():
+    page_url_list = []
+    base_url = 'https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&tg=0&date=20200405&page='
+    for i in range(1, 10 + 1):
+        page_url_list.append(base_url + str(i))
+    return page_url_list
+
+
+def get_url(page_url):
+    req = requests.get(page_url)
     soup = BeautifulSoup(req.text, 'html.parser')
     url_list = []
 
-    for movie_list in soup.select('div.tit3'):
-        url_list.append('https://movie.naver.com' + movie_list.a['href'])
+    for movie_list in soup.select('div.tit5'):
+        url_list.append(movie_list.a['href'])
     return url_list
 
 
-def get_item(movie_url):
-    genre = []  # 장음르
-    pub_year = '정보없'  # 개봉연도
+def get_item(url):
+    base_url = "https://movie.naver.com"
+    rating_list = ['전체 관람가', '12세 관람가', '15세 관람가', '청소년 관람불가']
+    genre = []  # 장르
+    pub_year = []  # 개봉연도
     actors = []  # 배우
     directors = []  # 감독
 
-    url = movie_url
-    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-    title = soup.select_one('h3.h_movie').find('a').getText()  # 타이틀
+    soup = BeautifulSoup(requests.get(base_url + url).text, 'html.parser')
+    try:
+        title = soup.select_one('h3.h_movie').find('a').getText()  # 타이틀
+    except AttributeError:
+        return None
     b_sub_title = soup.select_one('strong.h_movie2').getText()
-    title_english = (list(map(str.strip, b_sub_title.split(', ')))[0])  # 서브타이틀 / 영어제목
+    title_english = (list(map(str.strip, b_sub_title.split(', ')))[0])
     d1 = soup.select_one('dl.info_spec')
     s1 = d1.dt.findNextSibling('dd')
     temp = s1.find_all('a')
     temp2 = s1.find_all('span')
-    genre_dict = {
-        '1': '드라마', '2': '판타지', '3': '서부', '4': '공포', '5': '로맨스', '6': '모험', '7': '스릴러',
-        '8': '느와르', '9': '컬트', '10': '다큐멘터리', '11': '코미디', '12': '가족', '13': '미스터리',
-        '14': '전쟁', '15': '애니메이션', '16': '범죄', '17': '뮤지컬', '18': 'SF', '19': '액션',
-        '20': '무협', '21': '에로', '22': '서스펜스', '23': '서사', '24': '블랙코미디', '25': '실험',
-        '26': '영화카툰', '27': '영화음악', '28': '영화패러디포스터'
-    }
 
     movie_length = temp2[2].getText().strip()  # 상영시간
 
     for a in temp:
         p1 = parse.urlparse(a['href'])
         if 'genre' in p1[4]:
-            genre.append(genre_dict[p1.query[6:]])  # 장르
+            if int(p1.query[6:]) > 28:
+                return None
+            genre.append(p1.query[6:])  # 장르
 
-        if 'open' in p1[4]:
+        if 'open' in p1[4] and not pub_year:
             pub_year = p1[4][5:9]  # 개봉연도
-
     s2 = s1.findNextSibling('dd')
     s3 = s2.findNextSibling('dd')
     s4 = s3.findNextSibling('dd')
     for a in s2.p.select('a'):
-        directors.append(a.getText())
+        directors.append(a.getText())  # 감독
+
     for a in s3.p.select('a'):
         actors.append(a.getText())
+
     try:
         rating = s4.a.getText()  # 관람등급
+        if rating not in rating_list:
+            return None
     except AttributeError:
-        rating = '전체 관람가'
+        return None
     image_url = soup.select_one('div.poster').find('a').img['src']  # 포스터 url
     summary_text = soup.select("div.obj_section > div.video > div.story_area > p.con_tx")
     if summary_text:
@@ -76,4 +86,3 @@ def get_item(movie_url):
         'image_url': image_url,
         'summary': summary
     }
-
--- app/contents/management/commands/crawling.py
+++ app/contents/management/commands/crawling.py
@@ -1,45 +1,54 @@
-from django.core.management.base import BaseCommand
-from contents.management.commands.crawler import get_url, get_item
 import mimetypes
-import requests
+
 import magic
+import requests
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.management.base import BaseCommand
+
+from contents.management.commands.crawler import get_url, get_item, get_page_url
 from contents.models import Contents, Category, Actor, Director
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        url_list = get_url()
+        page_url_list = get_page_url()
+        all_url_list = []
+        for url in page_url_list:
+            all_url_list.append(get_url(url))
 
-        for url in url_list:
-            item = get_item(url)
-            contents, is_create = Contents.objects.get_or_create(contents_title=item['title'])
+        for url_list in all_url_list:
+            for url in url_list:
+                item = get_item(url)
+                if not item:
+                    continue
+                contents, is_create = Contents.objects.get_or_create(contents_title=item['title'])
 
-            if is_create:
-                response = requests.get(item['image_url'])
-                binary_data = response.content
-                mime_type = magic.from_buffer(binary_data, mime=True)
-                ext = mimetypes.guess_extension(mime_type)
-                file = SimpleUploadedFile(f'{item["title"]}{ext}', binary_data)
+                if is_create:
+                    response = requests.get(item['image_url'])
+                    binary_data = response.content
+                    mime_type = magic.from_buffer(binary_data, mime=True)
+                    ext = mimetypes.guess_extension(mime_type)
+                    file = SimpleUploadedFile(f'{item["title"]}{ext}', binary_data)
 
-                contents.contents_summary = item['summary']
-                contents.contents_title_english = item['title_english']
-                contents.contents_image = file
-                contents.contents_rating = item['rating']
-                contents.contents_length = item['length']
-                contents.contents_pub_year = item['pub_year']
-                contents.save()
+                    contents.contents_summary = item['summary']
+                    contents.contents_title_english = item['title_english']
+                    contents.contents_image = file
+                    contents.contents_rating = item['rating']
+                    contents.contents_length = item['length']
+                    contents.contents_pub_year = item['pub_year']
+                    contents.save()
 
-                for category in item['genre']:
-                    c1, _ = Category.objects.get_or_create(category_name=category)
+                    for category in item['genre']:
+                        c1, _ = Category.objects.get_or_create(category_name=category)
+                        contents.categories.add(c1)
 
-                for actor in item['actor']:
-                    a1, _ = Actor.objects.get_or_create(actor_name=actor)
-                    contents.actors.add(a1)
+                    for actor in item['actor']:
+                        a1, _ = Actor.objects.get_or_create(actor_name=actor)
+                        contents.actors.add(a1)
 
-                for director in item['director']:
-                    d1, _ = Director.objects.get_or_create(director_name=director)
-                    contents.directors.add(d1)
+                    for director in item['director']:
+                        d1, _ = Director.objects.get_or_create(director_name=director)
+                        contents.directors.add(d1)
 
         return self.stdout.write('크롤링 완료')
