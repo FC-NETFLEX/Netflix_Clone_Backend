be608cbe03e5050c84f1f83c0409189ce2d5e3b5
parkhongbeen <pack122@naver.com>
2020-04-08 19:43:04 +0900

    feat: 변수 이름 수정(스펠링 틀림), import 추가(Q)

--- app/contents/apis.py
+++ app/contents/apis.py
@@ -1,7 +1,7 @@
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from rest_framework import status, permissions, generics
-from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -77,9 +77,7 @@ class ContentsListView(APIView):
             category_name = request.query_params.get('category')
             all_contents = all_contents.filter(categories__category_name=category_name)
 
-        # all_contents.filter()
-
-        recommand_contents = all_contents[:10]
+        recommend_contents = all_contents[:10]
         watching_video = Watching.objects.filter(profile__id=profile_pk)
         top_contents = get_top_contents(all_contents)
         ad_contents = get_ad_contents(all_contents)
@@ -87,7 +85,7 @@ class ContentsListView(APIView):
         top10_contents = get_top10_contents(all_contents)
 
         serializer_all = ContentsSerializer(all_contents, many=True)
-        serializer_recommand = ContentsSerializer(recommand_contents, many=True)
+        serializer_recommend = ContentsSerializer(recommend_contents, many=True)
         serializer_top = ContentsDetailSerializer(top_contents, context={'profile_pk': profile_pk})
         serializer_ad = ContentsDetailSerializer(ad_contents, context={'profile_pk': profile_pk})
         serializer_watching_video = WatchingSerializer(watching_video, many=True)
@@ -98,7 +96,7 @@ class ContentsListView(APIView):
             "top_contents": serializer_top.data,
             "ad_contents": serializer_ad.data,
             "top10_contents": serializer_top10.data,
-            "recommand_contents": serializer_recommand.data,
+            "recommand_contents": serializer_recommend.data,
             "preview_contents": serializer_preview.data,
             "all_contents": serializer_all.data,
             "watcing_video": serializer_watching_video.data
