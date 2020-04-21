d7097e9f66e9ff20a1443af029de526c884ec348
hbyyy <qjaduddl94@gmail.com>
2020-04-22 01:40:58 +0900

    feat: profile RUD test code 추가

--- app/conftest.py
+++ app/conftest.py
@@ -1,3 +1,5 @@
+import uuid
+
 import pytest
 
 
@@ -25,6 +27,22 @@ def create_user(db, django_user_model):
 
 
 @pytest.fixture
+def create_profile(db, django_user_model, create_icon_category_and_icon):
+    from members.models import Profile
+
+    def make_profile(**kwargs):
+        if 'user' not in kwargs:
+            kwargs['user'] = create_user()
+        icon_list = create_icon_category_and_icon()
+
+        for icon in icon_list:
+            Profile.objects.create(user=kwargs['user'], profile_name=uuid.uuid4(), profile_icon=icon)
+        return Profile.objects.all()
+
+    return make_profile
+
+
+@pytest.fixture
 def create_icon_category_and_icon(db):
     import uuid
     import os
@@ -34,20 +52,20 @@ def create_icon_category_and_icon(db):
 
     def make_icon(**kwargs):
         image_path = os.path.join(ROOT_DIR, 'test_image.png')
-        for i in range(2):
-            ProfileIconCategory.objects.create(category_name=uuid.uuid4())
-        c1, c2 = ProfileIconCategory.objects.all()
-        for i in range(5):
 
-            icon1 = ProfileIcon.objects.create(icon_category=c1, icon_name=uuid.uuid4())
-            icon1.icon = SimpleUploadedFile(name='test_image.png', content=open(image_path, 'rb').read(),
-                                           content_type='image/png')
-            icon1.save()
+        c1 = ProfileIconCategory.objects.create(category_name=uuid.uuid4())
+        c2 = ProfileIconCategory.objects.create(category_name=uuid.uuid4())
 
-            icon2 = ProfileIcon.objects.create(icon_category=c2, icon_name=uuid.uuid4())
-            icon2.icon = SimpleUploadedFile(name='test_image.png', content=open(image_path, 'rb').read(),
-                                            content_type='image/png')
-            icon2.save()
+        icon1 = ProfileIcon.objects.create(icon_category=c1, icon_name=uuid.uuid4())
+        icon1.icon = SimpleUploadedFile(name='test_image.png', content=open(image_path, 'rb').read(),
+                                        content_type='image/png')
+        icon1.save()
+
+        icon2 = ProfileIcon.objects.create(icon_category=c2, icon_name=uuid.uuid4())
+        icon2.icon = SimpleUploadedFile(name='test_image.png', content=open(image_path, 'rb').read(),
+                                        content_type='image/png')
+        icon2.save()
+
         return ProfileIcon.objects.all()
 
     return make_icon
--- app/members/tests/test_profile.py
+++ app/members/tests/test_profile.py
@@ -2,6 +2,8 @@ import pytest
 from django.urls import reverse
 from rest_framework import status
 
+from members.models import Profile
+
 
 @pytest.mark.django_db
 class TestProfileAPI:
@@ -26,3 +28,39 @@ class TestProfileAPI:
 
         assert response.status_code == status.HTTP_200_OK
         assert len(response.data) == 1
+
+    def test_profile_retrieve_update_destroy(self, api_client, create_user, create_profile):
+        user, token = create_user(email='test@test.com')
+        profile_list = create_profile(user=user)
+
+        assert profile_list.union(Profile.objects.all(), all=False).count() == Profile.objects.count()
+        api_client.credentials(HTTP_AUTHORIZATION=f'TOKEN {token.key}')
+
+        # profile_retrieve test
+        for profile in profile_list:
+            url = reverse('members:profile-detail-update-destroy-view', kwargs={'pk': profile.pk})
+
+            response = api_client.get(url)
+            assert response.status_code == status.HTTP_200_OK
+            assert response.data.get('profile_name') == profile.profile_name
+
+        # profile_update test
+        for profile in profile_list:
+            url = reverse('members:profile-detail-update-destroy-view', kwargs={'pk': profile.pk})
+
+            data = {
+                'profile_name': 'test'
+            }
+            response = api_client.patch(url, data)
+            assert response.status_code == status.HTTP_200_OK
+            assert response.data.get('profile_name') == 'test'
+
+        # profile_delete test
+        for profile in Profile.objects.all():
+            url = reverse('members:profile-detail-update-destroy-view', kwargs={'pk': profile.pk})
+
+            response = api_client.delete(url)
+            assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+
