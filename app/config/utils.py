34198364bee4a25b7dc9db55956f3f144ac19ad5
hbyyy <qjaduddl94@gmail.com>
2020-04-22 20:42:44 +0900

    fix: Watching model 수정, IntegrityError exeption handling 수정
    1. Watching 모델에서 profile과 video를 unique_together로 설정
    2. IntegrityError 발생 시 에러 처리 핸들러 수정

--- app/config/utils.py
+++ app/config/utils.py
@@ -1,4 +1,6 @@
-from django.http import Http404
+from django.db import IntegrityError
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import exception_handler
 
 
@@ -10,5 +12,9 @@ def custom_exception_handler(exc, context):
     # Now add the HTTP status code to the response.
     if response is not None:
         response.data['status_code'] = response.status_code
+    if isinstance(exc, IntegrityError) and not response:
+        response = Response({
+            'error': 'IntegrityError',
+            'status_code': status.HTTP_400_BAD_REQUEST
+        })
     return response
-
