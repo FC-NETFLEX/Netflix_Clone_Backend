4dc27085eb856657a62242897c9b172387e7fc12
BeomYeongHeo <37058561+hbyyy@users.noreply.github.com>
2020-04-28 20:51:15 +0900

    Fix/refactoring (#107)
    * fix: profileicon이 삭제되었을 때, 그 프로필의 아이콘을 가지고 있는 프로필의 아이콘을 대표 아이콘 중 하나로 변경해도록 함
    
    * fix: members 모델들의 meta option 수정 (verbose_name), watching model의 constraints 설정
    
    * fix: members model의 verbose_name에  gettext_lazy 적용
    
    * feat: members app의 api에 주석 추가, 코드 정리
    
    * fix: Contents model verbose name 설정
    
    * fix: ContentsDerailSerializer 쿼리 수정, basicAuthentication 추기
    
    * fix: contents apis에 주석 추가
    
    * fix: utils.py 주석 추가

--- app/contents/utils.py
+++ app/contents/utils.py
@@ -5,6 +5,11 @@ from contents.models import Contents
 
 
 def get_ad_contents(queryset):
+    """
+    Contents의 queryset을 받아서 preview video가 존재하는 contents를 랜덤으로 1개 리턴
+    :param queryset: Contents queryset
+    :return: contents object
+    """
     contents_list = queryset.filter(preview_video__isnull=False)
     max_int = contents_list.count() - 1
     if max_int < 0:
@@ -17,6 +22,11 @@ def get_ad_contents(queryset):
 
 
 def get_top_contents(queryset):
+    """
+    Contents의 queryset을 받아서 2010년 이후 출시된 영화 중 로고가 있는 영화를 랜덤으로 1개 리턴
+    :param queryset: Contents queryset
+    :return: contents object
+    """
     contents_list = queryset.filter(contents_pub_year__gte='2010', contents_logo__isnull=False)
     max_int = contents_list.count() - 1
     if max_int < 0:
@@ -29,30 +39,26 @@ def get_top_contents(queryset):
 
 
 def get_preview_video(queryset):
-    contents_list = queryset.filter(preview_video__isnull=False,  contents_logo__isnull=False)
-    max_int = contents_list.count() - 1
-    if max_int < 0:
-        return
-    video_list = []
-    pk_list = []
-    while True:
-        if len(video_list) == max_int or len(video_list) == 10:
-            break
-        pk = random.randint(0, max_int)
-        if pk in pk_list:
-            continue
-        pk_list.append(pk)
-        video_list.append(contents_list[pk])
-    return video_list
+    """
+    Contents의 queryset을 받아서 preview_video가 있고 로고가 있는 contents를 랜덤으로 10개 리턴
+    :param queryset: Contents queryset
+    :return: Contents queryset
+    """
+    contents_list = queryset.filter(preview_video__isnull=False, contents_logo__isnull=False)
+    contents_list = contents_list.order_by('?')[:10]
+    return contents_list
 
 
 def get_popular_contents(queryset, count):
-    top10_list = []
+    """
+    like와 select 수가 많은 contents를 count 수 만큼 리턴
+    :param queryset: Contents queryset
+    :param count: count
+    :return: Contents queryset
+    """
     like_contents = queryset.filter(like_profiles__isnull=False)
     select_contents = queryset.filter(select_profiles__isnull=False)
     counter = (Counter(like_contents) + Counter(select_contents)).most_common(count)
-    for contents, _ in counter:
-        top10_list.append(contents)
-    pk_list = [contents.pk for contents in top10_list]
+    pk_list = [contents.pk for contents, _ in counter]
     top10_contents_queryset = Contents.objects.filter(pk__in=pk_list)
     return top10_contents_queryset
